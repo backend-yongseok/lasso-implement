21f0f445a88117176c9940ac18e35b6c959a75ca
ohdoski <yongseok.kim@linkareer.com>
2023-04-19 02:05:12 +0900

    :recycle: 리펙토링2

--- pages/convexHull.tsx
+++ pages/convexHull.tsx
@@ -1,66 +1,86 @@
-import React from "react";
-import Canvas from "./components/Canvas";
-
-interface Vertex {
-  x: number;
-  y: number;
-}
+import React from "react"
+import Canvas, { Point } from "./components/Canvas"
+import { useSetRecoilState } from "recoil"
+import { calcCountState, vertexLengthState } from "./stores/atoms"
 
 const Drawing: React.FC = () => {
-  const isPointInsidePolygon = (point: Vertex, polygon: Vertex[]): boolean => {
-    // Convex Hull을 구합니다.
-    const hull = getConvexHull(polygon);
+  const WIDTH = 1000
+  const HEIGHT = 1000
+  const setVertexLength = useSetRecoilState(vertexLengthState)
+  const setCount = useSetRecoilState(calcCountState)
 
-    // 주어진 점이 Convex Hull 내부에 있는지 검사합니다.
-    return isPointInsideConvexHull(point, hull);
-  };
+  const getConvexHull = (points: Point[]) => {
+    const sortedPoints = points.sort((a, b) => a.x - b.x)
 
-  const getConvexHull = (points: Vertex[]): Vertex[] => {
-    // x 좌표가 가장 작은 점을 기준으로 정렬합니다.
-    const sortedPoints = points.sort((a, b) => a.x - b.x);
+    const upperHull = buildHull(sortedPoints)
+    const lowerHull = buildHull(sortedPoints.reverse())
+    const result = upperHull.concat(lowerHull.slice(1, -1))
 
-    // 볼록 껍질을 만듭니다.
-    const upperHull = buildHull(sortedPoints);
-    const lowerHull = buildHull(sortedPoints.reverse());
+    return result
+  }
 
-    // 볼록 껍질을 합칩니다.
-    return upperHull.concat(lowerHull.slice(1, -1));
-  };
-
-  const buildHull = (points: Vertex[]): Vertex[] => {
-    const hull: Vertex[] = [];
+  const buildHull = (points: Point[]): Point[] => {
+    const hull: Point[] = []
 
     for (const point of points) {
       while (
         hull.length >= 2 &&
         ccw(hull[hull.length - 2], hull[hull.length - 1], point) <= 0
       ) {
-        hull.pop();
+        hull.pop()
       }
-      hull.push(point);
+      hull.push(point)
     }
 
-    return hull;
-  };
+    return hull
+  }
 
-  const isPointInsideConvexHull = (point: Vertex, hull: Vertex[]): boolean => {
-    // 볼록 껍질의 모든 선분과 주어진 점을 연결합니다.
-    const lineSegments = hull.map((p, i) => [p, hull[(i + 1) % hull.length]]);
+  const isPointInsideConvexHull = (point: Point, hull: Point[]): boolean => {
+    const lineSegments = hull.map((p, i) => [p, hull[(i + 1) % hull.length]])
 
-    // 주어진 점이 모든 선분의 왼쪽에 있는지 검사합니다.
-    return lineSegments.every(([a, b]) => ccw(a, b, point) >= 0);
-  };
+    return lineSegments.every(([a, b]) => ccw(a, b, point) >= 0)
+  }
 
-  // 세 점의 위치 관계를 구합니다. (ccw: counter clockwise)
-  const ccw = (a: Vertex, b: Vertex, c: Vertex): number => {
-    return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
-  };
+  const ccw = (a: Point, b: Point, c: Point): number => {
+    return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x)
+  }
+
+  const fillInsidePolygon = (
+    contextRef: React.MutableRefObject<CanvasRenderingContext2D | null>,
+    vertices: Point[]
+  ) => {
+    setVertexLength(vertices.length)
+    setCount(0)
+    if (!contextRef?.current || vertices.length === 0) {
+      return
+    }
+
+    const context = contextRef.current
+    const cols = WIDTH
+    const rows = HEIGHT
+    const hull = getConvexHull(vertices)
+
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols; col++) {
+        // setCount((prev) => prev + 1)
+
+        if (isPointInsideConvexHull({ x: col, y: row }, hull)) {
+          context.fillStyle = "#032134"
+          context.fillRect(col * 1, row * 1, 1, 1)
+        }
+      }
+    }
+  }
 
   return (
     <>
-      <Canvas isInside={isPointInsidePolygon}></Canvas>
+      <Canvas
+        width={WIDTH}
+        height={HEIGHT}
+        fillInsidePolygon={fillInsidePolygon}
+      ></Canvas>
     </>
-  );
-};
+  )
+}
 
-export default Drawing;
+export default Drawing
